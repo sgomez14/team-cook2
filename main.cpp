45f81cc32695032b297dfcfbc554d519636f8344
bmkuter <60325789+bmkuter@users.noreply.github.com>
2020-04-25 16:02:24 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    if (css.length()>0)
-    {
-        a.setStyleSheet(css);
-    }
+//    if (css.length()>0)
+//    {
+//        a.setStyleSheet(css);
+//    }
 
     HomePage w;
     w.show();
