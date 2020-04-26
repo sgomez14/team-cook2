6feb32d141c5ff52ee5a05d820f908d6050dee25
sgomez14 <santi09@bu.edu>
2020-04-26 14:10:26 -0400

    finalized homepage ui look

--- main.cpp
+++ main.cpp
@@ -1,29 +1,42 @@
 #include "homepage.h"
 #include "recipe_editor.h"
 #include <QDir>
+#include <QFile>
 #include <QString>
+#include <QTextStream>
 #include "cookbookFunctions.h"
 #include "recipe.h"
 
 
 #include <QApplication>
 
+
+QString readTextFile(QString path)
+{
+    QFile file(path);
+
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QTextStream inFile(&file);
+        return inFile.readAll();
+    }
+    return "";
+
+}
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+     QString css =readTextFile(":/resources/style/style.css");
 
-    /*if (css.length()>0)
+    if (css.length()>0)
     {
         a.setStyleSheet(css);
-    }*/
+    }
 
     initialise();
-
-//    if (css.length()>0)
-//    {
-//        a.setStyleSheet(css);
-//    }
 
 
     HomePage w;
